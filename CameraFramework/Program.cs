a8291ba1e87a851798d1f432805cbe9c719e5778
diana-vahomskaya <diana.vahomskaya@mail.ru>
2022-12-12 23:32:00 +0300

    added proto file, added code to proto file, it works))) left sending data using netmq

--- CameraFramework/Program.cs
+++ CameraFramework/Program.cs
@@ -1,6 +1,9 @@
 ﻿using NetMQ.Sockets;
 using ProtoBuf;
 using Google.Protobuf;
+using Emgu.CV;
+using System.Net.Sockets;
+using System.Net;
 
 namespace CameraFramework
 {
@@ -8,35 +11,76 @@ namespace CameraFramework
     {
         public static void Main()
         {
-            Console.WriteLine("Programm started");
-            var sender_img = new PushSocket("tcp://localhost:5557");//создаем push сокет
-                                                                    //new Emgu.CV.VideoCapture("http://guest:guest@192.168.9.37/video1.mjpg") для 528 аудитории
-            CapturedImage data = new CapturedImage();
-            using (Emgu.CV.VideoCapture capture = new Emgu.CV.VideoCapture()) //создаем захват видео 
+            var port = 5557;
+            string localIp = GetLocalIPAddress();
+            using (var sender_img = new PushSocket("tcp://" + localIp + ":" + port.ToString()))//создаем push сокет
             {
-                var capture_img = capture.QueryFrame();
-                var image = capture_img.ToImage<Emgu.CV.Structure.Bgr, byte>();
-                var imageData = image.ToJpegData(); //take a picture
-                data.Id = 1;
-                data.Time = DateTime.Now;
-                using (var str = new MemoryStream(imageData))
-                {
-                    data.Image = ByteString.FromStream(str);
-                }
-                //send объекта через zeromq
+                Console.WriteLine("Programm started");
+                Console.WriteLine("Local Ip:" + localIp);
+                Console.WriteLine("Port:" + port.ToString());
+                GetImgTest(false);
             }
         }
 
-    }
+        public static string GetLocalIPAddress()
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
 
+        public static FrameData GetImgTest(bool multy) //тест дома на ноуте
+        {
+            var count = 0;
+            bool one = true;
+            FrameData frame = new FrameData();
+            using (Emgu.CV.VideoCapture capture = new Emgu.CV.VideoCapture()) //подключаемся к видеокамере
+            {
+                while (one)
+                {
+                    var captured_img = capture.QueryFrame();
+                    var img = captured_img.ToImage<Emgu.CV.Structure.Bgr, byte>();
+                    var imgData = img.ToJpegData();
+                    frame.Version = 1;
+                    using (var str = new MemoryStream(imgData))
+                    {
+                        frame.Image = ByteString.FromStream(str);
+                    }
+                    frame.Time = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow);
+                    frame.FrameIndex = count;
+                    one = multy;
+                }
+            }
+            return frame;
+        }
 
-    [ProtoContract]
-    public class CapturedImage{
-        [ProtoMember(1)]
-        public int Id { get; set; }
-        [ProtoMember(2)]
-        public ByteString? Image { get; set; }
-        [ProtoMember(3)]
-        public DateTime Time { get; set; }
+        public static void GetImg()//фунция для камеры в 528 
+        {
+            var count = 0;
+            using (Emgu.CV.VideoCapture capture = new Emgu.CV.VideoCapture("http://guest:guest@192.168.9.37/video1.mjpg")) //подключаемся к видеокамере
+            {
+                while (true)
+                {
+                    FrameData frame = new FrameData();
+                    var captured_img = capture.QueryFrame();
+                    var img = captured_img.ToImage<Emgu.CV.Structure.Bgr, byte>();
+                    var imgData = img.ToJpegData();
+                    frame.Version = 1;
+                    using (var str = new MemoryStream(imgData))
+                    {
+                        frame.Image = ByteString.FromStream(str);
+                    }
+                    frame.Time = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.Now); 
+                    frame.FrameIndex = count;
+                    count++;
+                }
+            }
+        }
+
     }
 }
